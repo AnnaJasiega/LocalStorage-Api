f24f720754db6f7a21b42d0769c4d70f2039d99d
Anna Jasięga <ajasiega@gmail.com>
2021-02-21 12:00:19 +0100

    localStorage

--- src/app/car.service.ts
+++ src/app/car.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { DataPreparationService } from './data-preparation.service';
+import { Car } from './car/car';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  carList = [];
+  carList: Array<Car>= [];
   carLists = [];
   selected = [];
   carId: string;
@@ -16,11 +17,9 @@ export class CarService {
  */
   public getDataFromModel(url) {
     this.setCarList();
-    if (url === null) {
+    if (url === null || url === undefined) {
       return this.carList;
-    }else if (url === undefined)
-      return this.carList;
-    else{
+    } else{
       this.carId = this.service.prepareData(url)[0];
       this.owner = this.service.prepareData(url)[1];
 
@@ -47,51 +46,55 @@ export class CarService {
  * 
  */
   public addData(car){
+    this.setCarList();
     this.carList.push(car)
     window.localStorage.setItem('carList', JSON.stringify(this.carList))
     this.setCarList();
-  }
+  };
 
-  setCarList(){
-    this.carLists.push(JSON.parse(localStorage.getItem("carList")));
-    this.carList = this.carLists[0]
-  }
+  public setCarList(){
+    if(JSON.parse(localStorage.getItem("carList")) == null) {
+        this.carList = [];
+    } else{
+      this.carLists.push(JSON.parse(localStorage.getItem("carList")));
+      this.carList = this.carLists[0]
+    }
+  };
+
   public getCar(url){
     this.setCarList();
     this.carId = this.service.prepareData(url)[0];
     this.owner = this.service.prepareData(url)[1];
     this.selected = [];
 
-
     for (let i = 0; i<this.carList.length; i++){ 
-      if ((this.carList[i].id === this.carId) && (this.carList[i].owner === this.owner)){
-        this.selected.push(this.carList[i])
+      if ((this.carList[i]['id'] === this.carId) && (this.carList[i]['owner'] === this.owner)){
+        this.selected.push(this.carList[i]);
       }
     }
     return this.selected
-
-  }
+  };
 
   public findByData(carId?, owner?) {
     this.selected = [];
 
     if (carId && owner) {
       for (let car of this.carList){
-        if ((car.id === carId) && (car.owner === owner)){
+        if ((car['id'] === carId) && (car['owner'] === owner)){
           this.selected.push(car)
           return
         }
       }
     } else if (carId) {
       for (let car of this.carList){
-        if (car.id === carId){
+        if (car['id'] === carId){
           this.selected.push(car);
         }
       }
     }
     else if (owner) {
       for (let car of this.carList){
-        if (car.owner === owner){
+        if (car['owner'] === owner){
           this.selected.push(car);
         }
       }
